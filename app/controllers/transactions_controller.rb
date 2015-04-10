61021880422a698b4c0256783043148af001fe8c
Vesa Luusua <vesa.luusua@gmail.com>
2015-04-10 19:04:58 +0300

    locals as entity: transactions_controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -36,22 +36,36 @@ class TransactionsController < ApplicationController
 
     MarketplaceService::Transaction::Command.mark_as_seen_by_current(params[:id], @current_user.id)
 
-    show_total_price = tx[:payment_process] != :none
-    total_price_label = (tx[:payment_process] != :preauthorize) ? t("transactions.price") : t("transactions.total")
+    total_label = (tx[:payment_process] != :preauthorize) ? t("transactions.price") : t("transactions.total")
     unit_type = listing[:unit_type].present? ? translate_quantity_unit(tx[:unit_type]) : nil
+    quantity = tx[:listing_quantity]
+    show_subtotal = !!tx[:booking] || quantity.present? && quantity > 1) || tx[:shipping_price].present?
+
+    price_break_down_locals = TransactionViewUtils.price_break_down_locals({
+      listing_price: tx[:listing_price],
+      localized_unit_type: unit_type,
+      booking: !!tx[:booking],
+      start_on: booking ? tx[:booking][:start_on] : nil,
+      end_on: booking ? tx[:booking][:end_on] : nil,
+      duration: booking ? tx[:booking][:duration] : nil,
+      quantity: quantity,
+      subtotal: show_subtotal ? tx[:listing_price] * tx[:listing_quantity] : nil,
+      total: Maybe(tx[:payment_total].or_else(tx[:checkout_total]),
+      shipping_price: tx[:shipping_price],
+      show_total_price: show_total_price,
+      total_label: total_label
+    })
 
     render "transactions/show", locals: {
       messages: messages_and_actions.reverse,
       transaction: tx,
-      total_price_label: total_price_label,
-      show_total_price: show_total_price,
-      localized_unit_type: unit_type,
       listing: listing,
       transaction_model: tx_model,
       conversation_other_party: person_entity_with_url(conversation[:other_person]),
       is_author: listing.author_id == @current_user.id,
       message_form: MessageForm.new({sender_id: @current_user.id, conversation_id: conversation[:id]}),
-      message_form_action: person_message_messages_path(@current_user, :message_id => conversation[:id])
+      message_form_action: person_message_messages_path(@current_user, :message_id => conversation[:id]),
+      price_break_down_locals: price_break_down_locals
     }
   end
 
